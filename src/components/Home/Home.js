a012f8c1783855f8d7d793277841ce5ea4fadf4d
RituKotadiya <kotadiyaritu@gmail.com>
2025-06-03 12:25:31 +0530

    commit on 03/06/2025

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react';
 import "./Home.css"
 import image2 from "./weather-image.jpg";
 
-import bgVideo from "./video-1.mp4";
+import bgVideo from "./video-5.mp4";
+
 
 
 function Home() {
@@ -37,21 +38,15 @@ function Home() {
 
   return (
 
-    <div className="container-fluid d-flex justify-content-center align-items-center home-container ">
+    <div className="container-fluid d-flex justify-content-center align-items-center home-container  ">
 
       {/* <img src={image2} className="image" ></img> */}
-    <video autoPlay loop muted className="bg-video">
-  <source src={bgVideo} type="video/mp4" />
-</video>
+      <video autoPlay loop muted className="bg-video">
+        <source src={bgVideo} type="video/mp4" />
+      </video>
 
 
-
-
-
-
-
-
-      <div className="container text-center mt-5">
+      <div className="container text-center mt-0">
         <div className="search-box mb-4">
           <div className="input-group mb-3">
             <input type="text" className="form-control" placeholder="Enter a city name" value={city} onChange={(e) => setCity(e.target.value)} onKeyDown={(e) => e.key === 'Enter' && search(city)} />
@@ -61,27 +56,31 @@ function Home() {
           </div>
         </div>
 
+<div className="text-icon-box">
         <h4 className="mb-2">{weatherData.name}</h4>
         <p className="mb-2">{weatherData?.weather?.[0]?.description}</p>
 
-        {iconUrl && <img src={iconUrl} alt="હવામાન ચિહ્ન" className="weather-icon" />}
+        {iconUrl && <img src={iconUrl} className="weather-icon" />}
 
         <div className="icon display-4">{weatherData?.weather?.icon}</div>
         {/* <div className="icon display-4">🌧</div> */}
         <h2 className="my-3">{weatherData?.main?.temp}°C</h2>
 
-        <div className="container">
-          <div className="row justify-content-center ">
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
+        </div>
+
+        <div className="container ">
+       
+          <div className="row justify-content-center  gap-4 ">
+            <div className=" col-md-2">
+              <div className="p-1 temp-box rounded shadow-sm">
                 <span>
                   Min<br />
                   <span id="min">{weatherData?.main?.temp_min}°C </span>
                 </span>
               </div>
             </div>
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
+            <div className=" col-md-2">
+              <div className="  temp-box p-1 rounded shadow-sm">
                 <span>
                   Max<br />
                   <span id="max">{weatherData?.main?.temp_max}°C</span>
@@ -89,50 +88,52 @@ function Home() {
               </div>
             </div>
 
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                Humidity<br />
+            <div className=" col-md-2 ">
+              <div className=" temp-box p-1 rounded shadow-sm">
+               💧 Humidity<br />
                 <strong>{weatherData?.main?.humidity}%</strong>
               </div>
             </div>
-
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                sea_level<br />
+            <div className=" col-md-2">
+              <div className=" temp-box p-1 rounded shadow-sm">
+                🎚 Sealevel<br />
                 <strong>{weatherData?.main?.sea_level}%</strong>
               </div>
             </div>
-          </div>
+
+            </div>
         </div>
 
-        <div className="container">
-          <div className="row justify-content-center ">
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                grnd_level<br />
+        <div className="container mt-5">
+          <div className="row justify-content-center gap-4">
+            
+            <div className=" col-md-2">
+              <div className=" temp-box p-1 rounded shadow-sm">
+                ⛅ Grndlevel<br />
                 <strong>{weatherData?.main?.grnd_level}%</strong>
               </div>
             </div>
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                wind_speed<br />
+            <div className=" col-md-2">
+              <div className=" temp-box p-1 rounded shadow-sm">
+               💨 Windspeed<br />
                 <strong>{weatherData?.wind?.speed}%</strong>
               </div>
             </div>
 
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                sunrise<br />
+            <div className=" col-md-2">
+              <div className=" temp-box p-1 rounded shadow-sm">
+               ☀ Sunrise<br />
                 <strong>{weatherData?.sys?.sunrise}%</strong>
               </div>
             </div>
 
-            <div className=" col-md-3">
-              <div className="bg-light p-3 rounded shadow-sm">
-                sunrise<br />
+            <div className=" col-md-2">
+              <div className=" temp-box p-1 rounded shadow-sm">
+                🌥 Sunset<br />
                 <strong>{weatherData?.sys?.sunset}%</strong>
               </div>
             </div>
+          
           </div>
         </div>
 
